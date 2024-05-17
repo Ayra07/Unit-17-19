b58f889afd3a13be008e19a21fcfef6681119c45
Ayra07 <151808851+Ayra07@users.noreply.github.com>
2024-05-16 21:53:29 -0400

    New

---
+++ index.js
@@ -0,0 +1,37 @@
+console.log
+
+$(document).ready(function(){
+     $('.btn-primary2').mouseenter(function(){
+        $(this).css('background-color', '#CEE5E0');  
+        $(this).css('color', '#702632');  
+    });
+     
+    $('.btn-primary2').mouseleave(function(){
+        $(this).css('background-color', '#702632');
+        $(this).css('color', '#CEE5E0');   
+    });
+});
+
+$(document).ready(function(){
+    $('.btn-primary3').mouseenter(function(){
+       $(this).css('background-color', '#CEE5E0');  
+       $(this).css('color', '#702632');  
+   });
+    
+   $('.btn-primary3').mouseleave(function(){
+       $(this).css('background-color', '#702632');
+       $(this).css('color', '#CEE5E0');   
+   });
+});
+
+$(document).ready(function(){
+    $('.btn-primary1').mouseenter(function(){
+       $(this).css('background-color', '#CEE5E0');  
+       $(this).css('color', '#702632');  
+   });
+    
+   $('.btn-primary1').mouseleave(function(){
+       $(this).css('background-color', '#702632');
+       $(this).css('color', '#CEE5E0');   
+   });
+});
